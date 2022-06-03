b1a4fa4263293a6be65f86fcbccd976e5ab048e1
omar-bay <omarmb2002@gmail.com>
2022-06-03 15:39:49 +0300

    table created

--- models/estate_property.py
+++ models/estate_property.py
@@ -4,11 +4,11 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property Descriptions"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()  
