fe6c403fedb1eb1c36f846e8fc8b1e3dba4150d2
omar-bay <omarmb2002@gmail.com>
2022-06-04 21:17:49 +0300

    active field makes the record disapear if False

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from odoo import fields, models
 
 class EstateProperty(models.Model):
@@ -7,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.today())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()  
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,3 +22,4 @@ class EstateProperty(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
         # help="Where is the garden placed."
     )
+    active = fields.Boolean(default=False)
