b2434b1e72b5911180871a2208cec7bcad10fcf3
omar-bay <omarmb2002@gmail.com>
2022-06-05 16:46:28 +0300

    relational fields and their views

--- models/estate_property.py
+++ models/estate_property.py
@@ -31,3 +31,5 @@ class EstateProperty(models.Model):
         default='new'
     )
     property_type_id = fields.Many2one("estate.property.type", string="Type")
+    salesperson = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    buyer = fields.Char(copy=False)
