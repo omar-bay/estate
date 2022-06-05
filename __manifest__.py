571725edb2927df5b0baf8eabfec0f39ea5c995e
omar-bay <omarmb2002@gmail.com>
2022-06-05 14:11:15 +0300

    navigate to estate_property_type through a menu view under tag Settings

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,8 @@
         'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
-        'views/estate_menu_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_menu_views.xml',
     ],
     'application': True,
 }
