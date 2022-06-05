4e99c89336c034d160cdbf06c14ea2df1ab7ffb1
omar-bay <omarmb2002@gmail.com>
2022-06-05 19:58:40 +0300

    estate.property.offer model and views as tree in estate.property

--- models/estate_property.py
+++ models/estate_property.py
@@ -34,3 +34,4 @@ class EstateProperty(models.Model):
     salesperson = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     buyer = fields.Many2one("res.partner", copy=False)
     tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
