00e83cb08222da16c24df43735791a68b8d5461e
omar-bay <omarmb2002@gmail.com>
2022-06-07 10:47:43 +0300

    prevent type change in property view

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -3,10 +3,11 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "type of the property"
-    _order = "name"
+    _order = "sequence"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
+    sequence = fields.Integer()
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE (name)',
