718087bab75c9700f18b7717a24e937ddf9579a8
omar-bay <omarmb2002@gmail.com>
2022-06-07 11:32:42 +0300

    tag widgets color_picker and color shows

--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer(string="Color")
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE (name)',
